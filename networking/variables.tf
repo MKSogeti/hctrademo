35cb4128cd11dd4fa547169639a08f90a601259d
MKSogeti <113384569+MKSogeti@users.noreply.github.com>
2022-10-03 16:01:25 -0500

    Add files via upload

---
+++ networking/variables.tf
@@ -0,0 +1,21 @@
+# --- networking/variables.tf ---
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_cidrs" {
+  type = list(any)
+}
+
+variable "private_cidrs" {
+  type = list(any)
+}
+
+variable "access_ip" {
+  type = string
+}
+
+# variable "region" {
+#   type = string
+# }
